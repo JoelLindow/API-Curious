b7042365d4d351985dba7cec0fe75e12d707bb85
JoelLindow <joellindow@gmail.com>
2017-10-12 11:44:46 -0600

    navbar added. login options moved into dropdown

--- app/services/user_info.rb
+++ app/services/user_info.rb
@@ -1,8 +1,9 @@
 class UserInfo
-  attr_reader :username, :userimg, :userlocation, :useremail, :userbio
+  attr_reader :username, :userlogin, :userimg, :userlocation, :useremail, :userbio
 
   def initialize(user_info)
-    @username     = user_info[:login]
+    @username     = user_info[:name]
+    @userlogin    = user_info[:login]
     @userimg      = user_info[:avatar_url]
     @userlocation = user_info[:location]
     @useremail    = user_info[:email]
