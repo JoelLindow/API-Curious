5b464b3870c84e4537831951320e5e2d269f25c7
JoelLindow <joellindow@gmail.com>
2017-10-12 09:10:26 -0600

    navbar added. needs editing for basic layout

--- app/services/user_info.rb
+++ app/services/user_info.rb
@@ -1,11 +1,12 @@
 class UserInfo
-  attr_reader :username, :userimg, :userlocation, :useremail
+  attr_reader :username, :userimg, :userlocation, :useremail, :userbio
 
   def initialize(user_info)
     @username     = user_info[:login]
     @userimg      = user_info[:avatar_url]
     @userlocation = user_info[:location]
     @useremail    = user_info[:email]
+    @userbio      = user_info[:bio]
   end
 
 end
